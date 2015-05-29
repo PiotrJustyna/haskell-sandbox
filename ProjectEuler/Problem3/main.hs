5f2f4569809627999c639712b9e15f38f28a4691
Piotr Justyna <piotr.justyna@gmail.com>
2015-05-29 23:01:21 +0100

    Added comments to explain the new solution.

--- ProjectEuler/Problem3/main.hs
+++ ProjectEuler/Problem3/main.hs
@@ -5,3 +5,9 @@ solve forNumber withPotentialFactor
 
 main = do
     print (solve 600851475143 2)
+
+-- The solution is very performant now and amazingly simple. I had to do some reading and realised that the fundamental theorem of arithmetic (unique factorization theorem) was the solution here:
+--
+-- "every integer greater than 1 either is prime itself or is the product of prime numbers, and that this product is unique, up to the order of the factors".
+--
+-- Please read more here: http://en.wikipedia.org/wiki/Fundamental_theorem_of_arithmetic
