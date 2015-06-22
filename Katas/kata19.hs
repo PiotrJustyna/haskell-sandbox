c1e0355073f2d8c4a71a12b835dc1e4eec887d07
Piotr Justyna <piotr.justyna@gmail.com>
2015-06-22 19:54:41 +0100

    Minor modification - there's no need for "shortestPaths". Additionally, I replaced "foldr" with "foldr'".

--- Katas/kata19.hs
+++ Katas/kata19.hs
@@ -1,15 +1,16 @@
 import Data.List
+import Data.Foldable
 
-solve startWord endWord dictionary shortestPaths =
-    if (elem endWord relatedWords)
-        then ((endWord:startWord), (length startWord) + 1):shortestPaths
-        else if (any isWordRelated dictionary)
-            then foldr (\x acc -> acc ++ solve (x:startWord) endWord newDictionary shortestPaths) [] relatedWords
-            else shortestPaths
+solve startWord endWord dictionary =
+    if (Data.List.elem endWord relatedWords)
+        then [((endWord:startWord), (length startWord) + 1)]
+        else if (Data.List.any isWordRelated dictionary)
+            then foldr' (\x acc -> acc ++ solve (x:startWord) endWord newDictionary) [] relatedWords
+            else []
    where
        isWordRelated = (\x -> (length $ x \\ (head startWord)) == 1)
        relatedWords = filter isWordRelated dictionary
        newDictionary = filter (\x -> x /= (head startWord)) dictionary
 
 main = do
-    putStr $ show $ head $ sortBy (\x y -> if snd x > snd y then GT else LT) (solve ["cat"] "dog" ["cog", "dog", "mat", "cot", "cub", "cab", "bog", "meat", "bat", "beat", "bot"] [])
+    putStr $ show (solve ["cat"] "dog" ["cog", "dog", "mat", "cot", "cub", "cab", "bog", "meat", "bat", "beat", "bot"])
