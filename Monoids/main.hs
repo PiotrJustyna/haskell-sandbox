95ae75c886e26b17d3e001c61f0f479143f13404
Piotr Justyna <piotr.justyna@gmail.com>
2015-07-10 22:29:40 +0100

    Implemented tests for all Monoid laws.

--- Monoids/main.hs
+++ Monoids/main.hs
@@ -1,6 +1,6 @@
 import Data.Monoid
 
-data WeirdHomonym a = Pear a | Pair a a | DoesNotCompute deriving Show
+data WeirdHomonym a = Pear a | Pair a a | DoesNotCompute deriving (Show, Eq)
 
 instance Monoid a => Monoid (WeirdHomonym a) where
     mempty = DoesNotCompute
@@ -11,7 +11,20 @@ instance Monoid a => Monoid (WeirdHomonym a) where
     mappend (Pear _) (Pair _ _) = DoesNotCompute
     mappend (Pair _ _) (Pear _) = DoesNotCompute
 
+testMonoidLaw1 = mappend mempty x == x where x = Pear "Forelle"
+
+testMonoidLaw2 = mappend x mempty == x where x = Pear "Forelle"
+
+testMonoidLaw3 = mappend x (mappend y z) == mappend (mappend x y) z
+    where
+        x = Pear "Forelle"
+        y = Pear "Seckel"
+        z = Pear "Starkrimson"
+
 main = do
     print $ mappend (Pear "Green Anjou") (Pear "Concorde")
     print $ mappend (Pair "we" "do") (Pair "ll" "ne")
     print $ mappend (Pair "a" "b") (Pear "Bartlett")
+    print $ testMonoidLaw1
+    print $ testMonoidLaw2
+    print $ testMonoidLaw3
